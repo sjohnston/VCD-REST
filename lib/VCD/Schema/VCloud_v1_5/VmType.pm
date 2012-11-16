8841aeda93ed50bb532215c18d998c95157944c9
Stuart A Johnston <saj_git@thecommune.net>
2012-11-16 14:36:43 -0500

    Add sections to VmType.

--- lib/VCD/Schema/VCloud_v1_5/VmType.pm
+++ lib/VCD/Schema/VCloud_v1_5/VmType.pm
@@ -9,6 +9,11 @@ extends 'VCD::Schema::VCloud_v1_5::AbstractVAppType';
 has_xml_attr 'needsCustomization' => (is => 'ro', xml_is_optional => '1', xml_name => 'needsCustomization', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
 has_xml 'VAppScopedLocalId' => (is => 'ro', isa => 'Maybe[Str]', xml_maximum => '1', xml_minimum => '0', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
 has_xml 'Environment' => (is => 'rw', isa => 'Maybe[Str]', xml_maximum => '1', xml_minimum => '0', xml_namespace => 'http://schemas.dmtf.org/ovf/environment/1');
+has_xml 'VirtualHardwareSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::External::OVF::VirtualHardwareSectionType]', xml_namespace => 'http://schemas.dmtf.org/ovf/envelope/1');
+has_xml 'OperatingSystemSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::External::OVF::OperatingSystemSectionType]', xml_namespace => 'http://schemas.dmtf.org/ovf/envelope/1');
+has_xml 'NetworkConnectionSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::VCloud_v1_5::NetworkConnectionSectionType]', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
+has_xml 'GuestCustomizationSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::VCloud_v1_5::GuestCustomizationSectionType]', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
+has_xml 'RuntimeInfoSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::VCloud_v1_5::RuntimeInfoSectionType]', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
 
 
 __PACKAGE__->meta->make_immutable;
@@ -48,6 +53,22 @@ VM ID in scope of vApp.
 modifiable: always
 
 OVF environment section
+
+=item VirtualHardwareSection
+
+Element substitutable for Section since VirtualHardwareSection_Type is a
+derivation of Section_Type
+
+=item OperatingSystemSection
+
+Element substitutable for Section since OperatingSystemSection_Type is a
+derivation of Section_Type
+
+=item NetworkConnectionSection
+
+=item GuestCustomizationSection
+
+=item RuntimeInfoSection
 
 =back
 
