91335ac9a1286c53b0a05de64da55636fad41382
Stuart A Johnston <saj_git@thecommune.net>
2013-01-25 14:41:48 -0500

    Add array handles to task.

--- lib/VCD/Schema/VCloud_v1_5/TasksInProgressType.pm
+++ lib/VCD/Schema/VCloud_v1_5/TasksInProgressType.pm
@@ -6,7 +6,19 @@ use VCD::Schema;
 
 extends 'VCD::Schema::VCloud_v1_5::VCloudExtensibleType';
 
-has_xml 'Task' => (is => 'ro', isa => 'ArrayRef[VCD::Schema::VCloud_v1_5::TaskType]', xml_maximum => 'unbounded', xml_minimum => '0', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
+has_xml 'Task' => (is => 'ro', isa => 'ArrayRef[VCD::Schema::VCloud_v1_5::TaskType]', xml_maximum => 'unbounded', xml_minimum => '0', xml_namespace => 'http://www.vmware.com/vcloud/v1.5',
+    traits  => ['Array'],
+    handles => {
+        list  => 'elements',
+        push  => 'push',
+        map   => 'map',
+        grep  => 'grep',
+        find  => 'first',
+        get   => 'get',
+        count => 'count',
+        sort  => 'sort',
+    },
+);
 
 
 __PACKAGE__->meta->make_immutable;
