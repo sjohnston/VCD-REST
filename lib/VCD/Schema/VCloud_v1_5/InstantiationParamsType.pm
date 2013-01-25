cc61d7a7ee8d07c9bbf80d8560ceec2eb42af30f
Stuart A Johnston <saj_git@thecommune.net>
2013-01-25 16:33:03 -0500

    Add namespace

--- lib/VCD/Schema/VCloud_v1_5/InstantiationParamsType.pm
+++ lib/VCD/Schema/VCloud_v1_5/InstantiationParamsType.pm
@@ -7,8 +7,8 @@ use VCD::Schema;
 extends 'VCD::Schema::VCloud_v1_5::VCloudExtensibleType';
 
 has_xml 'Section' => (is => 'rw', isa => 'ArrayRef[VCD::Schema::External::OVF::SectionType]', xml_maximum => 'unbounded', xml_minimum => '0', xml_namespace => 'http://schemas.dmtf.org/ovf/envelope/1');
-has_xml 'NetworkConnectionSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::VCloud_v1_5::NetworkConnectionSectionType]', xml_maximum => '1', xml_minimum => '0');
-has_xml 'NetworkConfigSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::VCloud_v1_5::NetworkConfigSectionType]', xml_maximum => '1', xml_minimum => '0');
+has_xml 'NetworkConnectionSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::VCloud_v1_5::NetworkConnectionSectionType]', xml_maximum => '1', xml_minimum => '0', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
+has_xml 'NetworkConfigSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::VCloud_v1_5::NetworkConfigSectionType]', xml_maximum => '1', xml_minimum => '0', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
 
 
 __PACKAGE__->meta->make_immutable;
