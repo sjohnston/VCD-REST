1c615125dc0dc10c1e393f3880911777c56a7a20
Stuart A Johnston <saj_git@thecommune.net>
2013-02-05 14:50:27 -0500

    Add GuestCustomizationSection to InstantiationParams.

--- lib/VCD/Schema/VCloud_v1_5/InstantiationParamsType.pm
+++ lib/VCD/Schema/VCloud_v1_5/InstantiationParamsType.pm
@@ -9,6 +9,7 @@ extends 'VCD::Schema::VCloud_v1_5::VCloudExtensibleType';
 has_xml 'Section' => (is => 'rw', isa => 'ArrayRef[VCD::Schema::External::OVF::SectionType]', xml_maximum => 'unbounded', xml_minimum => '0', xml_namespace => 'http://schemas.dmtf.org/ovf/envelope/1');
 has_xml 'NetworkConnectionSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::VCloud_v1_5::NetworkConnectionSectionType]', xml_maximum => '1', xml_minimum => '0', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
 has_xml 'NetworkConfigSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::VCloud_v1_5::NetworkConfigSectionType]', xml_maximum => '1', xml_minimum => '0', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
+has_xml 'GuestCustomizationSection' => (is => 'rw', isa => 'Maybe[VCD::Schema::VCloud_v1_5::GuestCustomizationSectionType]', xml_maximum => '1', xml_minimum => '0', xml_namespace => 'http://www.vmware.com/vcloud/v1.5');
 
 
 __PACKAGE__->meta->make_immutable;
